a70243fcd160fdc04f40e7d900ae54c8f435790f
Imants Keiselis <keiselis96@gmail.com>
2021-03-26 01:35:39 +0200

    commit on 'master'

---
+++ Declare_JavaScript_Variables.js
@@ -0,0 +1,9 @@
+/*
+In computer science,
+ data is anything that is meaningful to the computer.
+  JavaScript provides eight different data types which are:
+   undefined, null, boolean, string, symbol, bigint, number, and object.*/
+
+   var ourName;
+   let name = 'Imants';
+   const age = 24;

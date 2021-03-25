f5037429955099d7df086380baeea6beabba21ec
Imants Keiselis <keiselis96@gmail.com>
2021-03-26 01:30:26 +0200

    first commit

---
+++ Comment_Your_JavaScript_Code.js
@@ -0,0 +1,9 @@
+// This is an in-line comment.
+/* This is a multi-line comment */
+
+
+//Try:
+
+//Welcome this code is taken from FreeCodeCamp
+/* I will be posting a lot of files with descriptive file_names!
+Have fun*/

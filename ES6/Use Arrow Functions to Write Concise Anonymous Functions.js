7e44e3a3ea788ce7512e1cafc12effc905648b3e
Imants Keiselis <keiselis96@gmail.com>
2021-03-30 14:17:33 +0300

    New batch 30 March

---
+++ ES6/Use Arrow Functions to Write Concise Anonymous Functions.js
@@ -0,0 +1,2 @@
+const magic = () => new Date();
+// declare what type of var/const/let name and function
---
+++ Questions.js
@@ -0,0 +1,21 @@
+var printNumTwo;
+for (var i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+// why it will print number 3 not 1 2 3 0
+let printNumTwo;
+for (let i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+console.log(i);
+// 2 and an error?

c61d4408389643c4ba1e4015410a632503791cd5
Imants Keiselis <keiselis96@gmail.com>
2021-03-31 06:38:42 +0300

    Commit

--- Questions.js
+++ Questions.js
@@ -1,21 +1,21 @@
-var printNumTwo;
-for (var i = 0; i < 3; i++) {
-  if (i === 2) {
-    printNumTwo = function() {
-      return i;
-    };
-  }
-}
-console.log(printNumTwo());
+// var printNumTwo;
+// for (var i = 0; i < 3; i++) {
+//   if (i === 2) {
+//     printNumTwo = function() {
+//       return i;
+//     };
+//   }
+// }
+// console.log(printNumTwo());
 // why it will print number 3 not 1 2 3 0
-let printNumTwo;
-for (let i = 0; i < 3; i++) {
+let printNum2;
+for (i = 0; i < 3; i++) {
   if (i === 2) {
-    printNumTwo = function() {
+    printNum2 = function() {
       return i;
     };
   }
 }
-console.log(printNumTwo());
+console.log(printNum2());
 console.log(i);
 // 2 and an error?
